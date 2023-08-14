7e40ebef84ce257754eaea7f202c27b8fe015690
Reyaz Ahmad <reyazoct@gmail.com>
2023-08-15 03:02:35 +0530

    Remove minify

--- boilerPlate/build.gradle.kts
+++ boilerPlate/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+//            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
